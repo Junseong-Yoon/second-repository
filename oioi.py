3e3d283b1c31ecb47c08c10e3b151828c365ebb3
Junseong-Yoon <wnstjd4085@gmail.com>
2025-11-09 02:26:22 +0900

    add. gitignore

---
+++ oioi.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 예시 데이터 만들기
+data = {
+    "이름": ["철수", "영희", "민수"],
+    "나이": [25, 22, 30],
+    "도시": ["서울", "부산", "인천"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+print("\n나이 평균:", df["나이"].mean())
